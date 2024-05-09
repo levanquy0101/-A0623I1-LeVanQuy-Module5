b12decedfb93bb9fce2558d5ed142cec80a1eb19
levanquy0101 <quyle.01012002@gmail.com>
2024-05-09 16:33:29 +0700

    up

---
+++ _05_React Form Router/practice/router/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+    return (
+      <>
+        <main>
+          <h2>Welcome to the homepage!</h2>
+        </main>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </>
+    );
+  }
+export default Home
